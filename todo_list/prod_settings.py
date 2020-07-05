9bbd66c4cf881cb1392f6b77ba12eb4b7ee70ed0
msdenimod <msdenimod@yandex.ru>
2020-07-05 13:40:05 +0400

    edit setting

--- todo_list/prod_settings.py
+++ todo_list/prod_settings.py
@@ -21,5 +21,5 @@ DATABASES = {
 }
 
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [STATIC_DIR]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [STATIC_DIR]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
