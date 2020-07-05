df0f725f8adec92cd58dae7c84247ceffa981e65
msdenimod <msdenimod@yandex.ru>
2020-07-05 15:23:11 +0400

    edit setting ДБ

--- todo_list/prod_settings.py
+++ todo_list/prod_settings.py
@@ -10,22 +10,22 @@ ALLOWED_HOSTS = ['*']
 
 EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend' # TOODO Надо посмотреть как отправлять письма
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'cm89591_todo',
-#         'USER': 'cm89591_todo',
-#         'PASSWORD': 'K11Sk3EL',
-#         'HOST': 'localhost',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cm89591_todo',
+        'USER': 'cm89591_todo',
+        'PASSWORD': 'K11Sk3EL',
+        'HOST': 'localhost',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
