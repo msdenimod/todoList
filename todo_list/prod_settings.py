ffd53ee4ea3f69197c59911007ee92c2ab00bbec
Denis <msdenimod@yandex.ru>
2021-02-17 05:30:42 +0000

    edit .gitignore

--- todo_list/prod_settings.py
+++ todo_list/prod_settings.py
@@ -6,7 +6,7 @@ SECRET_KEY = 'g=fvt8m5bumyv*!y13p$+0q5*3505__fet#xr9+!sk)($ih$0c'
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['todolistday.ru', '127.0.0.1']
 
 EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend' # TOODO Надо посмотреть как отправлять письма
 
@@ -15,18 +15,11 @@ DATABASES = {
        'ENGINE': 'django.db.backends.postgresql',
        'NAME': 'todo_db',
        'USER': 'todo',
-       'PASSWORD': 'Efedor44a//',
+       'PASSWORD': 'jYBpKUGL66nvDB',
        'HOST': '127.0.0.1',
        'PORT': '5432',
     }
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
